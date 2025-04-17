0b888503b0449649dead5f7ab09cd2f7fc695e01
Jerry Jiang <jerryjiang49@gmail.com>
2025-04-17 16:32:01 -0400

    feat: Implemented core URL shortener features
    - Integrated Redis for URL storage, click tracking, and TTL-based expiry
    - Error handling for missing or invalid URLs
    - Added an endpoint for URL code metadata

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
+import base64
+import validators
+import os
 import redis
-from fastapi import FastAPI
+import hashlib
+import datetime
+from http.client import HTTPException
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
 from starlette.responses import RedirectResponse
 
 app = FastAPI()
-r = redis.Redis(host='localhost', port=6379)
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-url_db = {}
+load_dotenv()
+BASE_URL = os.getenv('BASE_URL')
+base_url = os.getenv(f'{BASE_URL}', "http://127.0.0.1:8000/")
 
-@app.get("/")
-def get_url_db():
-     return url_db
+# SHA-256 - Reduces collision risk
+# Base64 - Encodes hash into a 6 character URL-friendly string
+def hash_url(url: str):
+     digest = hashlib.sha256(url.encode()).digest()
+     b64 = base64.urlsafe_b64encode(digest).decode()
+     return b64[:6].strip("=")
+
+@app.get("/api/urls/{url_code}")
+def get_url_metadata(url_code: str):
+     if not r.exists(f"{url_code}:url"):
+          raise HTTPException(status_code=404, detail="URL code not found")
+     clicks = r.get(f"{url_code}:clicks") or 0
+     return {
+          "original_url": r.get(f"{url_code}:url"),
+          "clicks": int(clicks),
+          "expiry_time": str(datetime.timedelta(seconds=r.ttl(f"{url_code}:url"))),
+          }
 
 @app.post("/shorten")
 def shorten_url(url: str):
-     short_code = str(hash(url))[:6]
-     url_db[short_code] = url
-     return {"Shortened URL: ", short_code}
+     if not validators.url(url):
+          raise HTTPException(status_code=400, detail="Invalid URL")
+     url_code = hash_url(url)
+     new_url = base_url + url_code
+     # Set key only if it doesn't exist with an expiration of 1 day
+     if r.set(f"{url_code}:url", url, nx=True, ex=60 * 60 * 24):
+          pass
+     return {
+          "short_url": new_url,
+          "short_code": url_code,
+     }
 
 @app.get("/{url_code}")
 def redirect(url_code: str):
-     return RedirectResponse(url_db.get(url_code))
+     if not r.get(f"{url_code}:url"):
+          raise HTTPException(status_code=404,detail="URL not found")
+
+     r.incr(f"{url_code}:clicks")
+     return RedirectResponse(r.get(f"{url_code}:url"))
+
 
 
